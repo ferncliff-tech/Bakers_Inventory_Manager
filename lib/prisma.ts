27bf0f66a96240534fb100301000f2b6b5b45653
Andrew Kruse <ferncliff.technology@gmail.com>
2026-05-02 10:27:54 -0700

    fix: add: only use postgresql

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,8 +1,6 @@
-import { PrismaClient as PrismaClientSqlite } from '../prisma/generated/sqlite'
 import { PrismaClient as PrismaClientPostgresql } from '../prisma/generated/postgresql'
 import { PrismaClient } from '@prisma/client'
 
-let prisma: PrismaClient = process.env.ENVIRONMENT === 'production' ?
- new PrismaClientPostgresql() : new PrismaClientSqlite()
+const prisma = new PrismaClientPostgresql()
 
 export default prisma
